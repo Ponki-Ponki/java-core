98f9469b63da6e70c3a0ef216601c55c0cdef628
Ponki-Ponki <vensop@gmail.com>
2023-09-18 19:57:36 +0300

    update homework3

---
+++ homework3/src/main/java/main/Freelancer.java
@@ -0,0 +1,15 @@
+package main;
+
+/**
+ * TODO: Доработать в рамках домашней работы
+ */
+public class Freelancer extends Employee {
+    protected Freelancer(String surName, String name, double salary) {
+        super(surName, name, salary);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+}
---
+++ homework3/src/main/java/main/Program.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = Worker.getEmployees(15);
+
+        for (Employee employee: employees) {
+            System.out.println(employee);
+        }
+
+        employees.sort(new EmployeeNameComparator());
+        System.out.println();
+
+        for (Employee employee: employees) {
+            System.out.println(employee);
+        }
+
+    }
+
+}
